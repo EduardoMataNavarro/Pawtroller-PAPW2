1e281d0fcaa2d5f7ab12021e6c349b790c6eceec
Eduardo I. Mata Navarro <ed_ward98_ed@hotmail.com>
2019-05-29 13:26:31 -0500

    Fixed bugs

--- resources/views/pages/pet.blade.php
+++ resources/views/pages/pet.blade.php
@@ -88,10 +88,10 @@
                                 </div>
                                 <hr>
                                 @endif
-                                </div>
-                            </div> -->
-                            <div class="row">
-                                <!-- <div class="col-sm-12 col-md-10 col-lg-8">
+                                </div> 
+                            </div>-->
+                            <!-- <div class="row">
+                                    <div class="col-sm-12 col-md-10 col-lg-8">
                                     <span class="h5"><a href="/profile/" target="_blank">Tito </a></span>
                                     -
                                     <span class="h6">Fecha</span>
@@ -109,9 +109,8 @@
                                             <span>Inbox</span>
                                         </div>
                                     </div>
-                                </div>
-                            </div> -->
-                            <hr>
+                                </div>-->
+                                 
                         </div>
                         <div class="tab-pane fade" id="fotos" role="tabpanel" aria-labelledby="fotos-tab">
                             <br>
